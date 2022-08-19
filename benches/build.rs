a73b2ff0541af422b7669c901b975ab60402be68
cavemanloverboy <caveycool@gmail.com>
2022-08-19 21:44:17 +0000

    add parallel build benchmark

--- benches/build.rs
+++ benches/build.rs
@@ -12,17 +12,22 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.sample_size(100);
 
     // Bench building tree
-    for ndata in [3, 4, 5].map(|p| 10_usize.pow(p)){
+    for ndata in [3, 4, 5, 6].map(|p| 10_usize.pow(p)){
 
         let data: Vec<[NotNan<f64>; D]> = (0..ndata)
             .map(|_| [(); D].map(|_| unsafe { NotNan::new_unchecked(rand::random()) } ))
             .collect();
 
+        for par_split in 1..3 {
+            group.bench_function(format!("Parallel({par_split}) Build (ndata = {ndata})").as_str(), |b| b.iter(|| {
+                let tree = Tree::new_parallel(black_box(&data), black_box(32), black_box(par_split)).unwrap();
+                drop(tree)
+            } ));
+        }
         group.bench_function(format!("Build (ndata = {ndata})").as_str(), |b| b.iter(|| {
             let tree = Tree::new(black_box(&data), black_box(32)).unwrap();
-            // println!("size = {}", tree.size())
             drop(tree)
-        } ));        
+        } ));     
     }
     group.finish();
 
