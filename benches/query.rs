5dc04d424dc9b718f614ca7ec0df8013ccd22bf7
cavemanloverboy <caveycool@gmail.com>
2022-08-19 06:59:44 +0000

    query bench

--- benches/query.rs
+++ benches/query.rs
@@ -3,12 +3,16 @@ use fnstw::Tree;
 use ordered_float::NotNan;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use num_format::{Locale, ToFormattedString};
 
 const D: usize = 3;
 
 
 fn criterion_benchmark(c: &mut Criterion) {
+
+    // rayon::ThreadPoolBuilder::new()
+    //     .num_threads(1)
+    //     .build_global();
 
     let mut group = c.benchmark_group("sample-size-example");
     group.sample_size(100);
@@ -24,11 +28,13 @@ fn criterion_benchmark(c: &mut Criterion) {
             .map(|_| [(); D].map(|_| unsafe { NotNan::new_unchecked(rand::random()) } ))
             .collect();
 
-        let tree = Tree::new(&data, 32).unwrap();
-        group.bench_function(format!("Query (ndata = {ndata})").as_str(), |b| b.iter(|| {
-            let v: Vec<_> = query
+        let tree = Tree::new(black_box(&data), black_box(32)).unwrap();
+        group.bench_function(
+            format!("{} queries (ndata = {})", QUERY.to_formatted_string(&Locale::en), ndata).as_str(),
+            |b| b.iter(|| {
+            let v: Vec<_> = black_box(&query)
                 .par_iter()
-                .map_with(&tree, |t, q| {
+                .map_with(black_box(&tree), |t, q| {
                     t.query_nearest(black_box(q))
                 })
                 .collect();
