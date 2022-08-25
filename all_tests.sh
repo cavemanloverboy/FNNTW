f196b862d00f8b584d8dcae1e21a566d4375ffd4
cavemanloverboy <caveycool@gmail.com>
2022-08-25 02:29:35 +0000

    renamed script

--- all_tests.sh
+++ all_tests.sh
@@ -1,10 +1,10 @@
 
 # External Validation Tests
-cargo run --release --example correctness_test
+cargo run --release --example correctness_test --quiet
 python examples/correctness_test.py
-cargo run --release --example correctness_test_periodic
+cargo run --release --example correctness_test_periodic --quiet
 python examples/correctness_test_periodic.py
-cargo run --release --example correctness_test_k
+cargo run --release --example correctness_test_k --quiet
 python examples/correctness_test_k.py
 
 # Internal Tests
